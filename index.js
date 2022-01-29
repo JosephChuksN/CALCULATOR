542b04f9a8e89b3ce3035d2c3d688fce97af56bc
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-01-29 20:27:26 +0100

    Calculator demo.

---
+++ index.js
@@ -0,0 +1,24 @@
+let outputScreen = document.getElementById("screen");
+
+
+function display(num){
+    outputScreen.value += num;
+}
+
+
+
+function calculate(){
+    try{
+        let input = outputScreen.value
+        let output = eval(input);
+        outputScreen.value = output;   
+    } catch(err){
+      outputScreen.value= "error";
+    }
+  
+}
+
+function Clr(){
+    outputScreen.value = " ";
+}
+
